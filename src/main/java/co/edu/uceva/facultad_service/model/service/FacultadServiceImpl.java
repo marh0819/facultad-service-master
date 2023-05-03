b23e8d7c0a785900c5e9fa9aa8e6fe6a40424e54
sebastian ospina <sebastina.ospina02@uceva.edu.co>
2023-05-03 14:43:17 -0500

    Se crearon los service de facultad

---
+++ src/main/java/co/edu/uceva/facultad_service/model/service/FacultadServiceImpl.java
@@ -0,0 +1,45 @@
+package co.edu.uceva.facultad_service.model.service;
+
+
+import co.edu.uceva.facultad_service.model.dao.IFacultadDao;
+import co.edu.uceva.facultad_service.model.entities.Facultad;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class FacultadServiceImpl implements IFacultadService{
+
+
+    @Autowired
+    IFacultadDao facultadDao;
+
+    @Override
+    public Facultad save(Facultad facultad) {
+        return facultadDao.save(facultad);
+    }
+
+    @Override
+    public void delete(Facultad facultad) {
+        facultadDao.delete(facultad);
+    }
+
+    @Override
+    public Facultad update(Facultad facultad) {
+        return facultadDao.save(facultad);
+    }
+
+    @Override
+    public Facultad findById(Long id) {
+        return facultadDao.findById(id).get();
+    }
+
+    @Override
+    @Transactional(readOnly = true)  //Para ejecutar en modo de solo lectura
+    public List<Facultad> findAll() {
+        List<Facultad> facultad = (List<Facultad>) facultadDao.findAll();
+        return facultad;
+    }
+}
---
+++ src/main/java/co/edu/uceva/facultad_service/model/service/IFacultadService.java
@@ -0,0 +1,18 @@
+package co.edu.uceva.facultad_service.model.service;
+
+import co.edu.uceva.facultad_service.model.entities.Facultad;
+
+import java.util.List;
+
+
+public interface IFacultadService {
+    Facultad save (Facultad facultad);
+
+    void delete (Facultad facultad);
+
+    Facultad update (Facultad facultad);
+
+    Facultad findById(Long id);
+
+    List<Facultad> findAll();
+}
