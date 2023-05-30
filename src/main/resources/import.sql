f7c2681301e1f04c1b0e73813cad9c28e9e3c571
seos1218 <sebasoapina1218@gmail.com>
2023-05-30 17:54:29 -0500

    Se insertaron todas la facultades de la UCEVA

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,6 @@
 INSERT INTO facultad (nombre) VALUES ('Facultad de Ciencias de la salud');
 INSERT INTO facultad (nombre) VALUES ('Facultad de Ingenieria');
+INSERT INTO facultad (nombre) VALUES ('Facultad de Ciencias Administrativas, Economicas y Contables');
+INSERT INTO facultad (nombre) VALUES ('Facultad de Ciencias Juridicas y Humanisticas');
+INSERT INTO facultad (nombre) VALUES ('Facultad de Ciencias de la Educacion');
+INSERT INTO facultad (nombre) VALUES ('Educacion Virtual a Distancia');
